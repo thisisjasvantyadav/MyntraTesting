5298b8ae5ad7bf25a83d02249b6733b0d26571d1
tanishapachpande05 <tanishapachpande0072@gmail.com>
2026-03-19 14:08:32 +0530

    [Tanisha] Added Home Page Test

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,12 +1,30 @@
 package pageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage{
 	
 	public HomePage(WebDriver driver) {
 		super(driver);
-		
 	}
+	
+	@FindBy(xpath="//span[normalize-space()='Profile']")
+	WebElement profileIcon;
+	
+	@FindBy(xpath="//input[@placeholder='Search for products, brands and more']")
+	WebElement searchBox;
+	
+	public boolean isProfileIconDisplayed() {
+		return (profileIcon.isDisplayed() && searchBox.isDisplayed());
+	}
+
+	public boolean isHomePageDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
 
 }
--- src/test/java/testCases/TC001_HomePageTest.java
+++ src/test/java/testCases/TC001_HomePageTest.java
@@ -1,5 +1,18 @@
 package testCases;
 
-public class TC001_HomePageTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import pageObjects.HomePage;
+import testBase.BaseClass;
+
+public class TC001_HomePageTest extends BaseClass {
+	
+	@Test
+	public void checkHomePageVisibility() {
+		HomePage homepage = new HomePage(driver);
+		boolean isHomePageDisplayed = homepage.isProfileIconDisplayed();
+		Assert.assertTrue(isHomePageDisplayed);
+	}
+	
 }
